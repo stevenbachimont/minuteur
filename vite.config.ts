944ca9eb56c8c0e213485cff9551a3455b352611
stevenbachimont <stevenbachimont@gmail.com>
2024-12-30 07:41:13 +0100

    page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: import.meta.env.BASE_URL || '/',
 })
